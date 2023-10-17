88a7ec87db9cd848d66a924d7ce245ccffb49f8e
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2023-10-17 23:58:16 +0530

    initial commit

--- src/Views/App/App.js
+++ src/Views/App/App.js
@@ -5,43 +5,46 @@ import ContactCard from './../../Component/ContactCard/ContactCard';
 
 
 function App() {
-  // console.log(phonebook);
+
   const [contact, setContact] = useState(phonebook);
-  const [searchTerm, setSearchTerm] = useState ('');
-  useEffect( () =>{
-    // console.log('searchTerm', searchTerm);
-    const filteredContact = phonebook.filter((contact)=>{
+  const [searchTerm, setSearchTerm] = useState('');
+ 
+  useEffect(() => {
+
+    const filteredContact = phonebook.filter((contact) => {
       const name = contact.name.toLowerCase();
       const mobail = contact.name.toString();
       const query = searchTerm.toLowerCase();
-      
+       
       return (name.includes(query) || mobail.includes(query))
-    })
+    });
     setContact(filteredContact);
-    
+    console.log(filteredContact);
+    console.log(searchTerm);
+
   }, [searchTerm]);
   return (
     <>
-    <div className="container">
-      <h1 className="heading">Search App </h1>
-      <input type="text"
-       placeholder="search"
-        className="search-input" 
-        value={searchTerm}
-        onChange={(e)=> {setSearchTerm(e.target.value)}}/>
+      <div className="container">
+        <h1 className="heading">Search App </h1>
+        <input type="text"
+          placeholder="search"
+          className="search-input"
+          value={searchTerm}
+          onChange={(e) => { setSearchTerm(e.target.value) }} />
 
         <h2 className="heading">Top 12 Best IT Company </h2>
 
 
-      <div className="tasks-container">
-        {contact.map((contact,index)=>{
-          const {  name, mobile} = contact;
-          return <ContactCard key={index} name={name} mobile={mobile} />
-        })}
-      </div>
-      <div className="footer">
-        Developed By <a className="git-footer" href="https://github.com/Abhijeetkokat007 ">@abhijeetkokat007<i class="fa-brands fa-github"></i></a>
-      </div>
+        <div className="tasks-container">
+          {contact.map((contact, index) => {
+            const { name, mobile } = contact;
+            return <ContactCard key={index} name={name} mobile={mobile} />
+          })}
+        </div>
+        <div className="footer">
+          Developed By <a className="git-footer" href="https://github.com/Abhijeetkokat007 ">@abhijeetkokat007<i class="fa-brands fa-github"></i></a>
+        </div>
       </div>
     </>
   );
